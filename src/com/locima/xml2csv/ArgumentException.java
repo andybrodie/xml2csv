8028b889ca6e20100a2a3aa7adfed5d6caafa323
Andy Brodie <andy@locima.com>
2014-09-30 17:34:17 +0100

    Just spotted that ArgumentException and ArgumentNullException are in testsrc instead of src!

---
+++ src/com/locima/xml2csv/ArgumentException.java
@@ -0,0 +1,28 @@
+package com.locima.xml2csv;
+
+/**
+ * Throws if an argument passed to a method was invalid, using a standardised error message.
+ */
+public class ArgumentException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = 1739962004704462299L;
+	
+	/**
+	 * Throws if an argument to a method was invalid with a default message.
+	 * @param argumentName the name of the argument / parameter than was invalid.
+	 */
+	public ArgumentException(String argumentName) {
+		super(argumentName +  " argument was invalid.");
+	}
+	
+	/**
+	 * Throws if an argument to a method was invalid.
+	 * @param argumentName the name of the argument / parameter than was invalid.
+	 * @param reason a reason as to why the argument was invalid.
+	 */
+	public ArgumentException(String argumentName, String reason) {
+		super(argumentName + " " + reason);
+	}
+	
+
+}
--- src/com/locima/xml2csv/ArgumentNullException.java
+++ src/com/locima/xml2csv/ArgumentNullException.java
@@ -1,9 +1,16 @@
 package com.locima.xml2csv;
 
+/**
+ * Thrown when an argument or parameter to a method was null when this is not allowed.
+ */
 public class ArgumentNullException extends ArgumentException {
 
 	private static final long serialVersionUID = 3495119751302269911L;
 	
+	/**
+	 * Thrown when an argument or parameter to a method was null when this is not allowed.
+	 * @param argumentName the name of the argument or parameter that was null.
+	 */
 	public ArgumentNullException(String argumentName) {
 		super(argumentName, "was null, which is not allowed.");
 	}
