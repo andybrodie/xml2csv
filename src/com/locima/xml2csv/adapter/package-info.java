a7ac913e60024b465d38d8e5255ac363c95bc0df
Andy Brodie <andy@locima.com>
2015-01-26 08:04:58 +0000

    Removed unused adapter package

--- src/com/locima/xml2csv/adapter/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Code within this package is responsible for managing the conversion between the results found within
- * {@link com.locima.xml2csv.extractor.AbstractExtractionContext} implementations and their output files.
- */
-package com.locima.xml2csv.adapter;
